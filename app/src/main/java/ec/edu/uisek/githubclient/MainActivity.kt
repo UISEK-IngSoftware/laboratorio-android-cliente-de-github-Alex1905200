78fba8cff215e6fd1369b18a1ccc35fbe3a1ff49
Laboratorio2 <alexander.montero@uisek.edu.ec>
2025-11-05 11:47:33 -0500

    Laboratorio5 Alexander

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,8 +1,16 @@
 package ec.edu.uisek.githubclient
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
+import ec.edu.uisek.githubclient.services.GithubApiService
+import ec.edu.uisek.githubclient.services.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,17 +18,61 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-       setupRecyclerView()
 
+        setupRecyclerView()
+        binding.repoFab.setOnClickListener {
+            displayNewRepoForm()
+        }
     }
 
+    override fun onResume() {
+        super.onResume()
+        fetchRepositories()
+    }
 
     private fun setupRecyclerView() {
         reposAdapter = ReposAdapter()
         binding.reposRecyclerView.adapter = reposAdapter
     }
+
+    private fun fetchRepositories() {
+        val apiService: GithubApiService = RetrofitClient.gitHubApiService
+        val call = apiService.getRepos()
+
+        call.enqueue(object: Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>?>, response: Response<List<Repo>?>) {
+                if(response.isSuccessful) {
+                    val repos = response.body()
+                    if (repos != null && repos.isNotEmpty()) {
+                        reposAdapter.updateRepositories(repos)
+                    } else {
+                        showMessage("No se encontraron repositorios")
+                    }
+                } else {
+                    val errorMessage = when (response.code()) {
+                        401 -> "No Autorizado"
+                        403 -> "Prohibido"
+                        404 -> "No Encontrado"
+                        else -> "Error ${response.code()}"
+                    }
+                    showMessage ("Error: $errorMessage")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
+                showMessage("No se pudieron cargar los repositorio")
+            }
+        })
+    }
+
+    private fun showMessage (message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+    private fun displayNewRepoForm() {
+        Intent(this, RepoForm::class.java).apply {
+            startActivity(this)
+        }   }
 }
--- app/src/main/java/ec/edu/uisek/githubclient/Repoitem.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/Repoitem.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import ec.edu.uisek.githubclient.databinding.FragmentRepoitemBinding
 
-
 /**
  * A simple [Fragment] subclass.
  * Use the [Repoitem.newInstance] factory method to
@@ -15,7 +14,7 @@ import ec.edu.uisek.githubclient.databinding.FragmentRepoitemBinding
  */
 class Repoitem : Fragment() {
     private var _binding: FragmentRepoitemBinding? = null
-    private val binding get()= _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,24 +27,28 @@ class Repoitem : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRepoitemBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_repoitem, container, false)
+        _binding = FragmentRepoitemBinding.inflate(inflater,container,false)
         return binding.root
     }
 
-override  fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    binding.repoName.text="Mi repositorio"
-    binding.repoDescription.text="Este es mi repositorio"
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.repoName.text = "Mi repositorio"
+        binding.repoDescription.text = "Descripción del repositorio"
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
-         *
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
